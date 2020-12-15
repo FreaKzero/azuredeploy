a49fd42f7b1c767f95c20339e87bf0e09483e4d8
FreaKzero <pete@freakzero.com>
2020-12-15 10:15:09 +0100

    add envtest string

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: process.env.ENVTEST || 'No ENVTEST var found' });
 });
 
 module.exports = router;
